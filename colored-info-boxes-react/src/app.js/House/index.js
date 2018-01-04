d885ea833243f3f7d5f9a13e7d7d7e12a5da24fb
Samantha Skidmore <samslife2016@gmail.com>
2018-01-03 17:44:51 -0700

    newest updates

---
+++ colored-info-boxes-react/src/app.js/House/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+function Colorbox(props) {
+    return (
+            <span class = "blue">blue</span>
+    )
+}
+export default Colorbox;
+
+//when ready, run the app in terminal by typing:  "npm start"
---
+++ colored-info-boxes-react/src/app.js/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+function (props) {
+    return (
+        let house = [
+            {
+                livingRoom: [
+                    "Couch",
+                    "Chair",
+                    "Rug"],
+            },
+
+            {
+                diningRoom: [
+                    "Table",
+                    "Chairs",
+                    "Buffet"],
+            },
+
+            {
+                kitchen: [
+                    "Cabinets",
+                    "Counters",
+                    "Appliances"],
+            },
+
+            {
+                laundryRoom: [
+                    "Washer",
+                    "Dryer",
+                    "Folding Table"],
+            },
+
+            {
+                office: [
+                    "Desk",
+                    "Chair",
+                    "Filing Cabinet"],
+            },
+
+            {
+                master: [
+                    "Bed",
+                    "Dresser",
+                    "Chaise"],
+            },
+
+            {
+                teenBedroom: [
+                    "Bed",
+                    "Dresser",
+                    "Posters"],
+            },
+
+            {
+                babyRoom: [
+                    "Crib",
+                    "Changing Table",
+                    "Rocking Chair"],
+            },
+
+            {
+                garage: [
+                    "Car",
+                    "Tools",
+                    "Shelfs"],
+            },
+
+            {
+                backyard: [
+                    "Patio Table",
+                    "Patio Chairs",
+                    "Fire Pit"],
+            },
+        ]
+          
+    )
+}
+export default app;
+
+//when ready, run the app in terminal by typing:  "npm start"
+
